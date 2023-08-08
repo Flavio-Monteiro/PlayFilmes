a8c9bfabce4929f2ae9bd2afc9447504338efb40
Flávio Monteiro <flavio.ms@outlook.com.br>
2023-08-08 20:48:00 -0300

    feito hover adcionado

--- src/Components/CommonQuestions.jsx
+++ src/Components/CommonQuestions.jsx
@@ -17,7 +17,7 @@ const CommonQuestions = () => {
             <h1>Perguntas Frequentes</h1>
             
             <div className={`faq-item ${activeItem === 1 ? 'active' : ''}`} onClick={() => handleItemClick(1)}>
-                <div className="faq-question">O que é a PlaayTV?</div>
+                <div className="faq-question">O que é a PlayTV?</div>
                 {activeItem === 1 && (
                     <div className="faq-answer">
                         A PlayTV é um serviço de streaming que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet.
@@ -31,8 +31,10 @@ const CommonQuestions = () => {
                 <div className="faq-question">Quanto custa a PlayTV?</div>
                 {activeItem === 2 && (
                     <div className="faq-answer">
-                        Assista à PlayTV no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única.
-                        Os planos variam de R$18,90 a R$55,90 por mês. Sem contrato nem taxas extras.
+                        <p>
+                            Assista à PlayTV no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única.
+                            Os planos variam de R$18,90 a R$55,90 por mês. Sem contrato nem taxas extras.
+                        </p>
                     </div>
                 )}
             </div>
@@ -43,12 +45,14 @@ const CommonQuestions = () => {
                 <div className="faq-question">Onde posso assistir?</div>
                 {activeItem === 3 && (
                     <div className="faq-answer">
-                        Assista onde quiser, quando quiser.
-                         Faça login com sua conta PlayTV em playtv.com para começar a assistir no computador 
-                         ou em qualquer aparelho conectado à Internet compatível com o aplicativo PlayTV, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
-                        Você também pode baixar a sua série favorita com o aplicativo PlayTV para iOS,
-                         Android ou Windows 10. Use downloads para levar a PlayTV para onde quiser sem precisar de conexão com a Internet.
-                          Leve o PlayTV com você para qualquer lugar.
+                        <p>
+                            Assista onde quiser, quando quiser.
+                             Faça login com sua conta PlayTV em playtv.com para começar a assistir no computador
+                             ou em qualquer aparelho conectado à Internet compatível com o aplicativo PlayTV, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
+                            Você também pode baixar a sua série favorita com o aplicativo PlayTV para iOS,
+                             Android ou Windows 10. Use downloads para levar a PlayTV para onde quiser sem precisar de conexão com a Internet.
+                              Leve o PlayTV com você para qualquer lugar.
+                        </p>
                     </div>
                 )}
             </div>
@@ -57,9 +61,11 @@ const CommonQuestions = () => {
                 <div className="faq-question">Como faço para cancelar?</div>
                 {activeItem === 4 && (
                     <div className="faq-answer">
-                        A PlayTV é flexível. Não há contratos nem compromissos.
-                        Você pode cancelar a sua conta na internet com apenas dois cliques.
-                        Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.
+                        <p>
+                            A PlayTV é flexível. Não há contratos nem compromissos.
+                            Você pode cancelar a sua conta na internet com apenas dois cliques.
+                            Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.
+                        </p>
                     </div>
                 )}
             </div>
@@ -68,12 +74,11 @@ const CommonQuestions = () => {
                 <div className="faq-question">O que eu posso assistir na PlayTV?</div>
                 {activeItem === 5 && (
                     <div className="faq-answer">
-                        A PlayTV tem um grande catálogo de filmes, documentários, séries, originais PlayTV premiados e muito mais. Assista o quanto quiser, quando quiser.
+                        <p>A PlayTV tem um grande catálogo de filmes, documentários, séries, originais PlayTV premiados e muito mais. Assista o quanto quiser, quando quiser.</p>
                     </div>
                 )}
             </div>
         </div>
     )
-}
-
-export { CommonQuestions }
+} 
+export {CommonQuestions}
